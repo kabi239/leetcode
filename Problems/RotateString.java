3c0aa00ed6c33eef09f1ba2ba2e72db402e1e6ca
kabi239 <kavyasharma239@gmail.com>
2025-02-11 23:31:52 +0530

    Problem no 796

---
+++ Problems/RotateString.java
@@ -0,0 +1,21 @@
+public class RotateString {
+    public static String shiftCharToEnd(String s){
+        if(s.length()<=1){
+            return s;
+        }
+        return s.substring(1)+s.charAt(0);
+    }
+    public boolean rotateString(String s, String goal) {
+        int i=0;
+        while(i< s.length()){
+            if(s.equals(goal)){
+                return true;
+            }
+            else{
+                s=shiftCharToEnd(s);
+            }
+           i++; 
+        }
+        return false;
+    }
+}
